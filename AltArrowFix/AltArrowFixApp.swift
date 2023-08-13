4881cd1f93832f96321992880791efdb7c14746c
jayadamsmorgan <applejay1802@gmail.com>
2023-08-13 10:38:35 -0400

    Initial Commit

---
+++ AltArrowFix/AltArrowFixApp.swift
@@ -0,0 +1,17 @@
+//
+//  AltArrowFixApp.swift
+//  AltArrowFix
+//
+//  Created by user on 8/13/23.
+//
+
+import SwiftUI
+
+@main
+struct AltArrowFixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
